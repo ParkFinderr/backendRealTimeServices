968b5eb6163cec0924416bdc7902f6f8454ede55
ivalhad <ivan14hadrian@gmail.com>
2026-02-11 14:48:23 +0700

    add services init for redis

---
+++ src/services/redisService.js
@@ -0,0 +1,15 @@
+const { createClient } = require('redis');
+const config = require('../config/config');
+
+const initRedis = async () => {
+  const subscriber = createClient({ url: `redis://${config.redisHost}:6379` });
+  
+  subscriber.on('error', (err) => console.error('[REDIS ERROR]', err));
+  
+  await subscriber.connect();
+  console.log('✅ Terhubung ke Redis Subscriber');
+  
+  return subscriber;
+};
+
+module.exports = { initRedis };
