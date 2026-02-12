7677899bff6a2a2441888717d91a327b4e7af0de
ivalhad <ivan14hadrian@gmail.com>
2026-02-12 11:03:52 +0700

    refactor: enhance MQTT message handling for slot actions and improve logging

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,47 +1,73 @@
+// backendRealTimeServices/src/handlers/eventHandler.js
 const config = require('../config/config');
 
 const setupEventHandlers = (redisSubscriber, mqttClient, io) => {
   
-  //  listen redis
+  const redisPublisher = redisSubscriber.duplicate();
+  redisPublisher.connect().then(() => {
+    console.log('Publisher khusus sensor berjalan.');
+  });
+
+  // fungsi mengirim ke mqtt
   redisSubscriber.subscribe(config.redisChannelCmd, (message) => {
     try {
       const payload = JSON.parse(message);
       console.log(`[REDIS] Cmd: ${payload.action} -> ${payload.slotName}`);
 
-      const targetTopic = `parkfinder/control/${payload.slotName}`;
       let mqttMessage = '';
+      const targetTopic = `parkfinder/control/${payload.slotName}`;
 
       switch (payload.action) {
+    
         case 'reserveSlot': mqttMessage = 'setReserved'; break;
+  
         case 'occupySlot': mqttMessage = 'setOccupied'; break;
+   
         case 'leaveSlot': 
-        case 'cancelSlot': 
-          mqttMessage = 'setAvailable'; break;
-        case 'maintenanceSlot': mqttMessage = 'setOccupied'; break;
+        case 'cancelSlot': mqttMessage = 'setAvailable'; break;
+   
+        case 'maintenanceSlot': mqttMessage = 'setOccupied'; break; 
+  
         case 'alertSlot': mqttMessage = 'buzzerOn'; break;
       }
 
       if (mqttMessage) {
         mqttClient.publish(targetTopic, mqttMessage);
-        io.emit('updateMap', payload);
+        console.log(`[MQTT OUT] Kirim ke ${targetTopic} : ${mqttMessage}`);
       }
-    } catch (err) {
-      console.error('[RedisError]', err);
-        }
+
+    } catch (error) {
+      console.error('[REDIS ERROR] Gagal parse pesan:', error);
+    }
   });
 
-  // listen mqtt
-  mqttClient.on('message', (topic, message) => {
+  // mengirim data daris sensor 
+  mqttClient.on('message', async (topic, message) => {
+
     if (topic.includes('control')) return;
 
     const msgString = message.toString();
-    console.log(`[MQTT] ${topic}: ${msgString}`);
+    console.log(`[MQTT IN] ${topic}: ${msgString}`);
 
     io.emit('sensorUpdate', {
       topic: topic,
       message: msgString,
       timestamp: new Date().toISOString()
     });
+
+    try {
+        const slotName = topic.split('/').pop(); 
+        
+        const sensorPayload = {
+            slotName: slotName,
+            value: msgString 
+        };
+
+        await redisPublisher.publish('parkfinderSensorUpdate', JSON.stringify(sensorPayload));
+        
+    } catch (err) {
+        console.error('Gagal kirim update sensor:', err.message);
+    }
   });
 };
 
