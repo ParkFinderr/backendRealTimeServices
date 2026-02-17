7c95c38f1674dbfb58be77465923b1bcbccb63d7
ivalhad <ivan14hadrian@gmail.com>
2026-02-17 16:16:03 +0700

    update config for ci/cd pipeline

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -5,32 +5,40 @@ let subscriber = null;
 let publisher = null;
 
 const getConnectionUrl = () => {
-  const host = process.env.REDIS_HOST
-  const port = process.env.REDIS_PORT
+  const host = process.env.REDIS_HOST || '127.0.0.1';
+  const port = process.env.REDIS_PORT || 6379;
+  const password = process.env.REDIS_PASSWORD;
+
+  if (password) {
+    return `redis://:${password}@${host}:${port}`;
+  }
+  
   return `redis://${host}:${port}`;
 };
 
 const connectSubscriber = async () => {
   const url = getConnectionUrl();
-  console.log(`[REDIS SUB] terhubung ke ${url}...`);
+  
+  console.log(`[REDIS SUB] Menghubungkan ke Redis host: ${process.env.REDIS_HOST}...`);
   
   subscriber = createClient({ url });
   subscriber.on('error', (err) => console.error('[REDIS SUB] Error:', err));
   
   await subscriber.connect();
-  console.log('[REDIS SUB] Terhubung ');
+  console.log('[REDIS SUB] Terhubung');
   return subscriber;
 };
 
 const connectPublisher = async () => {
   const url = getConnectionUrl();
-  console.log(`[REDIS PUB] terhubung ke ${url}...`);
+  
+  console.log(`[REDIS PUB] Menghubungkan ke Redis host: ${process.env.REDIS_HOST}...`);
 
   publisher = createClient({ url });
   publisher.on('error', (err) => console.error('[REDIS PUB] Error:', err));
   
   await publisher.connect();
-  console.log('[REDIS PUB] terhubung');
+  console.log('[REDIS PUB] Terhubung');
   return publisher;
 };
 
